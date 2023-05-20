2703f61edfaa59153450ccd81728d1e5bda3a387
MoonSangWon <kuim75@naver.com>
2023-05-20 14:28:42 +0900

    feat : 멤버 delete api 추가

--- src/main/java/mega/waka/controller/WakaController.java
+++ src/main/java/mega/waka/controller/WakaController.java
@@ -1,6 +1,7 @@
 package mega.waka.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import mega.waka.service.MemberService;
 import mega.waka.service.WakaTimeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,16 +14,18 @@ import java.util.UUID;
 public class WakaController {
 
     private final WakaTimeService wakaTimeService;
+    private final MemberService memberService;
 
-    public WakaController(WakaTimeService wakaTimeService) {
+    public WakaController(WakaTimeService wakaTimeService, MemberService memberService) {
         this.wakaTimeService = wakaTimeService;
+        this.memberService = memberService;
     }
 
     @PostMapping("/{name}")
     @Operation(summary = "Create Member API", description = "새로운 멤버를 추가합니다.")
     public ResponseEntity createMember(@PathVariable String name, @RequestParam String organization, @RequestParam String apiKey) {
         try{
-            wakaTimeService.add_Member_By_apiKey(name, organization, apiKey);
+            memberService.add_Member_By_apiKey(name, organization, apiKey);
             return new ResponseEntity("success", HttpStatus.OK);
         }catch (RuntimeException e){
             return ResponseEntity.badRequest().body(e.getMessage());
@@ -44,7 +47,7 @@ public class WakaController {
 
             return new ResponseEntity(wakaTimeService.get_Member_Time_By_ApiKey(date), HttpStatus.OK);
         }catch (RuntimeException e){
-            return new ResponseEntity("fail", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity(e.fillInStackTrace(), HttpStatus.BAD_REQUEST);
         }
     }
     @PostMapping("/members/{id}")
@@ -52,10 +55,20 @@ public class WakaController {
     public ResponseEntity updateApiKey(@PathVariable String id, @RequestParam String apiKey){
         try{
             UUID uuid = UUID.fromString(id);
-            wakaTimeService.update_apiKey(uuid, apiKey);
+            memberService.update_apiKey(uuid, apiKey);
             return new ResponseEntity("success", HttpStatus.OK);
         }catch (RuntimeException e){
-            return new ResponseEntity("fail", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity(e.fillInStackTrace(), HttpStatus.BAD_REQUEST);
+        }
+    }
+    @DeleteMapping("/members/{id}")
+    @Operation(summary = "delete member API", description = "멤버를 삭제합니다.")
+    public ResponseEntity deleteMember(@PathVariable String id){
+        try{
+            memberService.delete_member(UUID.fromString(id));
+            return new ResponseEntity("해당 멤버가 삭제되었습니다.", HttpStatus.OK);
+        }catch (RuntimeException e){
+            return new ResponseEntity(e.fillInStackTrace(), HttpStatus.BAD_REQUEST);
         }
     }
 }
