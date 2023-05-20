7ad61cc927b2b1add7ecb5514d00cb42975c4c27
MoonSangWon <kuim75@naver.com>
2023-05-20 14:14:44 +0900

    feat : CORS Config add

---
+++ src/main/java/mega/waka/config/WebConfig.java
@@ -0,0 +1,18 @@
+package mega.waka.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("*").maxAge(3600);
+    }
+}
