989283fa3fa9113bfea922f8bea1bdd34b1e9638
MoonSangWon <kuim75@naver.com>
2023-06-09 21:38:34 +0900

    feat : 개인순위 조회시 이름과 다를경우 로직 추가

--- src/main/java/mega/waka/discord/DiscordListener.java
+++ src/main/java/mega/waka/discord/DiscordListener.java
@@ -3,12 +3,14 @@ package mega.waka.discord;
 import mega.waka.entity.Member;
 import mega.waka.repository.MemberRepository;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.awt.*;
@@ -23,6 +25,8 @@ import java.util.regex.Pattern;
 public class DiscordListener extends ListenerAdapter {
 
     private final MemberRepository memberRepository;
+    @Value("${discord.bot.channel}")
+    private String channelId;
 
     public DiscordListener(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
@@ -31,8 +35,9 @@ public class DiscordListener extends ListenerAdapter {
     public void createMessage(MessageReceivedEvent event, String message){
         User user = event.getAuthor();
         String returnMessage = "";
+        String newMessage = "*******근무 시간 미달자 ******* \n";
         EmbedBuilder embed = new EmbedBuilder();
-        embed.setFooter("와카보드 봇", "https://dev.megabrain.kr/waka");
+        embed.setFooter("와카보드 봇 https://dev.megabrain.kr/waka", "https://avatars.githubusercontent.com/inje-megabrain");
         switch(message){
             case "명령어" : returnMessage = "** 와카보드 봇 명령어 목록입니다.**\n waka! 전체순위 : 와카보드 전체 순위를 보여줍니다. \n waka! 개인순위 : 와카보드 개인 순위를 보여줍니다. \n waka! 명령어 : 와카보드 봇 명령어를 보여줍니다.";
             break;
@@ -52,14 +57,38 @@ public class DiscordListener extends ListenerAdapter {
                 Collections.sort(sortedList, Map.Entry.comparingByValue(Comparator.reverseOrder()));
                 embed.setTitle(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))+" 기준 " +"와카보드 전체 순위");
                 embed.setColor(Color.green);
+                int cnt=0;
                 for(int i=0;i<sortedList.size();i++){
-                    returnMessage += (i+1)+"위 "+sortedList.get(i).getKey()+" "+sortedList.get(i).getValue()/60+"시간 "+sortedList.get(i).getValue()%60+"분\n";
+                    if(sortedList.get(i).getValue()/60 <=10*60+59){
+                        cnt++;
+                        newMessage += (i+1)+"위 "+sortedList.get(i).getKey()+" "+sortedList.get(i).getValue()/60+"시간 "+sortedList.get(i).getValue()%60+"분\n";
+                    }
+                    else returnMessage += (i+1)+"위 "+sortedList.get(i).getKey()+" "+sortedList.get(i).getValue()/60+"시간 "+sortedList.get(i).getValue()%60+"분\n";
                 }
-                embed.setDescription(returnMessage);
+                if(cnt==0) newMessage += "현재 근무 시간 미달자가 없습니다.\n";
+                embed.setDescription(returnMessage + newMessage);
                 returnMessage = "현재 시간 기준 전체 순위입니다.";
             break;
             case "개인순위" :
-                Member member = memberRepository.findByName(user.getName().substring(2));
+                Member member;
+                if(user.getName().equals("웅이")) member = memberRepository.findByName("신종웅");
+                else if(user.getName().equals("jadru"))  member = memberRepository.findByName("박영건");
+                else if(user.getName().equals("김동현hry")) member = memberRepository.findByName("김동현");
+                else if(user.getName().equals("LeeByeongJin")) member = memberRepository.findByName("이병진");
+                else if(user.getName().equals("Rod12")) member = memberRepository.findByName("이욱진");
+                else if(user.getName().equals("jyh")) member = memberRepository.findByName("정용휘");
+                else if(user.getName().equals("23김우현")) member = memberRepository.findByName("김우현");
+                else if(user.getName().equals("병석")) member = memberRepository.findByName("성병석");
+                else if(user.getName().equals("훈정이")) member = memberRepository.findByName("김훈정");
+                else if(user.getName().equals("보경")) member = memberRepository.findByName("문보경");
+                else if(user.getName().equals("Zn")) member = memberRepository.findByName("심아연");
+                else if(user.getName().equals("Rod12")) member = memberRepository.findByName("이욱진");
+                else if(user.getName().equals("재민")) member = memberRepository.findByName("김재민");
+                else{
+                    member = memberRepository.findByName(user.getName());
+                }
+
+
                 if(member ==null) returnMessage = "해당 멤버가 등록되지 않았습니다.";
                 else{
                     List<Member> memberList2 = memberRepository.findAll();
@@ -73,16 +102,22 @@ public class DiscordListener extends ListenerAdapter {
                             memberMap2.put(member.getName(),hour*60+minute);
                         }
                     }
+                    int cnt2=0;
                     List<Map.Entry<String,Integer>> sortedList2 = new ArrayList<>(memberMap2.entrySet());
                     Collections.sort(sortedList2, Map.Entry.comparingByValue(Comparator.reverseOrder()));
                     embed.setTitle(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))+" 기준 " +"와카보드 개인 순위");
                     embed.setColor(Color.green);
                     for(int i=0;i<sortedList2.size();i++){
-                        if(sortedList2.get(i).getKey().equals(user.getName().substring(2))){
-                            returnMessage += (i+1)+"위 "+sortedList2.get(i).getKey()+" "+sortedList2.get(i).getValue()/60+"시간 "+sortedList2.get(i).getValue()%60+"분\n";
+                        if(sortedList2.get(i).getKey().equals(user.getName())){
+                            if(sortedList2.get(i).getValue() <=10*60+59) {
+                                cnt2++;
+                                newMessage += (i+1)+"위 "+sortedList2.get(i).getKey()+" "+sortedList2.get(i).getValue()/60+"시간 "+sortedList2.get(i).getValue()%60+"분\n";
+                            }
+                            else returnMessage += (i+1)+"위 "+sortedList2.get(i).getKey()+" "+sortedList2.get(i).getValue()/60+"시간 "+sortedList2.get(i).getValue()%60+"분\n";
                         }
                     }
-                    embed.setDescription(returnMessage);
+                    if(cnt2==0) newMessage += "현재 근무 시간 미달자가 없습니다.\n";
+                    embed.setDescription(returnMessage + newMessage);
                     returnMessage = "현재 시간 기준 순위입니다.";
                 }
             break;
@@ -98,6 +133,7 @@ public class DiscordListener extends ListenerAdapter {
     }
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
+        if(!event.getChannel().getId().equals(channelId)) return;
         User user = event.getAuthor();
         TextChannel textChannel = event.getChannel().asTextChannel();
         Message message = event.getMessage();
