ad5af0545a99cf9b2eaf00d5f1b7ac1c849c644d
MoonSangWon <kuim75@naver.com>
2023-10-22 20:20:47 +0900

    fix : Change Discord channel id

--- src/main/java/mega/waka/discord/DiscordListener.java
+++ src/main/java/mega/waka/discord/DiscordListener.java
@@ -36,7 +36,7 @@ public class DiscordListener extends ListenerAdapter {
     }
     @Override
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {  //test->1116650046797135992, 1090659127417638943
-        if(!event.getChannel().getId().equals("1116650046797135992")) return;
+        if(!event.getChannel().getId().equals("1090659127417638943")) return;
         super.onSlashCommandInteraction(event);
 
         User user = event.getUser();
